06808f97a52343ff74984eb57d33d9518f1ade43
StevenMalla01 <stvnmalla@gmail.com>
2021-12-18 19:45:24 -0500

    primary commit

---
+++ resources/views/recetas/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('botones')
+    <a href={{route('recetas.create')}} class="btn btn-primary mr-2 text-white">Crear Recetas</a>
+@endsection
+@section('content')
+<h2 class="text-center mb-5"> Administra tus recetas</h2>
+<div class="col-md-10 mx-auto bg-white p-3">
+    <table class="table">
+        <thead class="bg-primary text-light">
+
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Categoria</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Pizza de camarones</td>
+                <td>Pizzas</td>
+                <td>Pizza de queso</td>
+
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+
+@endsection
